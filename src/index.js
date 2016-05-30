f717232460bc9cc639a88734512f73278b7d9301
Christian Muñoz <christian.munoz@flow.io>
2016-05-29 21:20:07 -0400

    Refactor project source code to ES6 syntax

--- src/index.js
+++ src/index.js
@@ -1,35 +1,22 @@
-'use strict';
+import { assign, includes, get, last, omit } from 'lodash';
+import { transform } from 'babel-core';
 
-var _ = require('lodash');
-var babel = require('babel-core');
+const defaults = { extensions: ['js'] };
 
-var defaults = {
-    extensions: ['js']
-};
-
-function shouldProcessFile (filename, options) {
-    var extensions = options.extensions;
-    var parts = filename.split('.');
-
-    if (parts.length) {
-        return (extensions.indexOf(parts[parts.length - 1]) >= 0);
-    } else {
-        return false;
-    }
+function shouldProcessFile(filename, { extensions }) {
+  return includes(extensions, last(filename.split('.')));
 }
 
-function process (source, options) {
-    var result = babel.transform(source, _.omit(options, 'extensions'));
-    return result.code;
+function process(source, options) {
+  return transform(source, omit(options, 'extensions')).code;
 }
 
-exports.handlers = {
+export const handlers = {
+  beforeParse: (event) => {
+    const options = assign(defaults, get(global, 'env.conf.babel'));
 
-    beforeParse: function (event) {
-        var options = _.assign(defaults, _.get(global, 'env.conf.babel'));
-
-        if (shouldProcessFile(event.filename, options)) {
-            event.source = process(event.source, options);
-        }
+    if (shouldProcessFile(event.filename, options)) {
+      event.source = process(event.source, options); // eslint-disable-line
     }
+  },
 };
--- test/specs/index.js
+++ test/specs/index.js
@@ -1,47 +1,42 @@
-var _ = require('lodash');
-var babel = require('babel-core');
-var plugin = require('../../src/index');
+import { set } from 'lodash';
+import * as babel from 'babel-core';
+import { handlers } from '../../src/index';
 
-var expect = chai.expect;
+describe('babel parser', () => {
+  const sandbox = sinon.sandbox.create();
+  const source = 'const { attribute } = object;';
 
-describe('babel parser', function () {
-
-  var sandbox = sinon.sandbox.create();
-
-  var source = 'const { attribute } = object;';
-  var transpiled = 'var attribute = object.attribute;';
-
-  _.set(global, 'env.conf.babel', {
+  set(global, 'env.conf.babel', {
     extensions: ['js', 'jsx'],
-    presets: ['es2015']
+    presets: ['es2015'],
   });
 
-  beforeEach(function () {
+  beforeEach(() => {
     sandbox.spy(babel, 'transform');
   });
 
-  afterEach(function () {
+  afterEach(() => {
     sandbox.restore();
   });
 
   it('should transpile source code', () => {
-    var event = { source: source, filename: '/path/to/file.js' };
-    plugin.handlers.beforeParse(event);
-    expect(event.source).to.equal(babel.transform(source, { presets: ['es2015'] }).code);
+    const event = { source, filename: '/path/to/file.js' };
+    const transpiled = babel.transform(source, { presets: ['es2015'] }).code;
+    handlers.beforeParse(event);
+    expect(event.source).to.equal(transpiled);
   });
 
-  it('should not transpile files that do not match configured extensions', function () {
-    var event = { source: source, filename: '/path/to/file.ufo' };
-    plugin.handlers.beforeParse(event);
+  it('should not transpile files that do not match configured extensions', () => {
+    const event = { source, filename: '/path/to/file.ufo' };
+    handlers.beforeParse(event);
     expect(babel.transform).to.not.have.been.called;
     expect(event.source).to.equal(source); // not transpiled
   });
 
-  it('should pass options to babel transformer without extensions', function () {
-    var event = { source: source, filename: '/path/to/file.jsx' };
-    plugin.handlers.beforeParse(event);
+  it('should pass options to babel transformer without extensions', () => {
+    const event = { source, filename: '/path/to/file.jsx' };
+    handlers.beforeParse(event);
     expect(babel.transform).to.have.been.calledOnce;
     expect(babel.transform).to.have.been.calledWithExactly(source, { presets: ['es2015'] });
   });
-
 });
