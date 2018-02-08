d9d82447622b357de8f0b926b323db7dd6f08876
yoav@wix.com <yoav@wix.com>
2018-02-08 15:23:35 +0200

    added code to preserve the line numbers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { assign, includes, get, last, omit } from 'lodash';
 import { transform } from 'babel-core';
+import jsdocRegex from 'jsdoc-regex';
 
 const defaults = { extensions: ['js'] };
 
@@ -7,20 +8,57 @@ function shouldProcessFile(filename, { extensions }) {
   return includes(extensions, last(filename.split('.')));
 }
 
-function processFile(source, options) {
+function countCharInRange(source, from, to, char) {
+  let cnt = 0;
+  for (let i = from; i < to; i += 1) {
+    if (source[i] === char) {
+      cnt += 1;
+    }
+  }
+  return cnt;
+}
+
+function stripWhitespace(text) {
+  return text.replace(/ /g, '');
+}
+
+function processFile(source, options, doclets) {
+  const regex = jsdocRegex();
+  let lastIndex = 0;
+  let linesCount = 1;
+  let match = regex.exec(source);
+  while (match !== null) {
+    const { index } = match;
+    linesCount += countCharInRange(source, lastIndex, index, '\n');
+    const key = stripWhitespace(match[0]);
+    // eslint-disable-next-line no-param-reassign
+    doclets[key] = linesCount;
+    lastIndex = index;
+    match = regex.exec(source);
+  }
   return transform(source, omit(options, 'extensions')).code;
 }
 
+let doclets = {};
 // eslint-disable-next-line import/prefer-default-export
 export const handlers = {
   beforeParse: (event) => {
+    doclets = {};
     const options = assign(defaults, {
       filename: event.filename,
     }, get(global, 'env.conf.babel'));
 
     if (shouldProcessFile(event.filename, options)) {
       // eslint-disable-next-line no-param-reassign
-      event.source = processFile(event.source, options);
+      event.source = processFile(event.source, options, doclets);
+    }
+    console.log(doclets);
+  },
+  newDoclet: (e) => {
+    if (e) {
+      if (doclets[stripWhitespace(e.doclet.comment)]) {
+        e.doclet.meta.lineno = doclets[stripWhitespace(e.doclet.comment)];
+      }
     }
   },
 };
